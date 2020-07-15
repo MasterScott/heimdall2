fdc4310cb6c0e36f07a7dea588dd9dcadfa7636f
Robert Clark <rbclark@mitre.org>
2020-07-15 13:40:24 -0400

    Add /server route so the heimdall frontend can detect whether or not it should be in server mode

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,10 @@ import {AuthzModule} from './authz/authz.module';
 import {AuthnModule} from './authn/authn.module';
 import {ServeStaticModule} from '@nestjs/serve-static';
 import {join} from 'path';
+import {AppController} from './app.controller';
 
 @Module({
+  controllers: [AppController],
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client', 'dist'),
